edd17fc12e3481e89dd2944a97732244a820abb3
messabih-khalil <msbih.khalil@gmail.com>
2022-11-21 17:07:13 +0100

    fix navbar style

--- src/components/DownloadButton.svelte
+++ src/components/DownloadButton.svelte
@@ -1,26 +1,20 @@
-<!-- <script>
-    import file from '../assets/pokecss.zip'
-</script> -->
-
-<!-- download button -->
-
-<!-- {file} -->
+<script>
+	import file from '../assets/pokecss.zip';
+</script>
 
 <a
-	
 	class="bg-yellow-400 download-button font-600 pdl-10 pdr-10 pdt-2 pdb-2 flex align-center br-2x-solid-black black"
-	href='*'
-    download="pokecss.zip"
+	href={file}
+	download="pokecss.zip"
 >
 	<i class="ri-download-2-line mgr-1" />
 	<span> Download </span>
 </a>
 
-
 <style>
 	.download-button {
-			font-size: 1.2rem;
-		}
+		font-size: 1.2rem;
+	}
 	@media only screen and (max-width: 720px) {
 		.download-button {
 			display: none;
